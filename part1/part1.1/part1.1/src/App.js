64423b26edb6db993f14774b2fb168fc9043c6d4
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-27 19:50:59 +1100

    Exercise complete

--- part1/part1.1/part1.1/src/App.js
+++ part1/part1.1/part1.1/src/App.js
@@ -9,17 +9,24 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <header course = {course}>
+        <h1>{course}</h1>
+      </header>
+      <content>  
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      </content>
+      <total>
+
+      </total>
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }

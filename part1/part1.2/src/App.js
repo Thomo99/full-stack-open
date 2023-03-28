58d00e073f62c5f5442e7acbbae547de93ab823b
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-03-28 20:11:41 +1100

    1.5 complete

--- part1/part1.2/src/App.js
+++ part1/part1.2/src/App.js
@@ -1,32 +1,33 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <header>
-        <h1>{course}</h1>
+        <h1>{course.name}</h1>
       </header>
       <content>
-          {parts.map(part => (
+          {course.parts.map(part => (
             <Content part = {part.name} exercises = {part.exercises} />
           ))}
        </content>
       <total>
-        <p>Number of exercises {parts.reduce ((total, part) => total + part.exercises, 0 )}</p>
+        <p>Number of exercises {course.parts.reduce ((total, part) => total + part.exercises, 0 )}</p>
       </total>
         
     </div>

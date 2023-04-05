022d4c0994efcfae64e05515af65e9b1345ef887
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-06 05:03:47 +1000

    Theory complete

--- part2/theory1/src/App.js
+++ part2/theory1/src/App.js
@@ -1,15 +1,15 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-          <li key={note.id}>
-            {note.content}
-          </li>
-        )}
+        <ul>
+          {notes.map(note => 
+            <Note key={note.id} note={note} />
+          )}
+        </ul>
       </ul>
     </div>
   )

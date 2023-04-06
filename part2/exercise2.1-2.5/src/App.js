144e29bac1f9ffc8eda3365ea546ac0181c49b42
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-07 06:05:28 +1000

    Single line App() made with header working

--- part2/exercise2.1-2.5/src/App.js
+++ part2/exercise2.1-2.5/src/App.js
@@ -1,35 +1,36 @@
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3,
       }
     ]
   }
-  return (
+  return <Course course = {course}/>
+
+  
+}
+
+const Course = ({course}) => {
+  return(
     <div>
       <header>
-        <h1>{course.name}</h1>
+      <h1>{course.name}</h1>
       </header>
-      <content>
-          {course.parts.map(part => (
-            <Content part = {part.name} exercises = {part.exercises} />
-          ))}
-       </content>
-      <total>
-        <p>Number of exercises {course.parts.reduce ((total, part) => total + part.exercises, 0 )}</p>
-      </total>
-        
     </div>
   )
 }
@@ -45,3 +46,20 @@ const Content = (props) => {
 }
 
 export default App
+
+
+{/* <total>
+        <p>Number of exercises {course.parts.reduce ((total, part) => total + part.exercises, 0 )}</p>
+      </total> */}
+      // <div>
+    //   <header>
+    //     <h1>{course.name}</h1>
+    //   </header>
+      
+    //       {course.parts.map(part => (
+    //         <Content key = {part.id} part = {part.name} exercises = {part.exercises} partId = {part.id}/>
+    //       ))}
+       
+      
+        
+    // </div>

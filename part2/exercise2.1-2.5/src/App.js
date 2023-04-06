8998bfe7f693f12cdcf1cee94b555e766f5dd6de
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-07 06:25:09 +1000

    2.4 complete w/no errors. Still confused. Need to practice map

--- part2/exercise2.1-2.5/src/App.js
+++ part2/exercise2.1-2.5/src/App.js
@@ -1,31 +1,55 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      }
-    ]
-  }
-  return <Course course = {course}/>
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+    {courses.map(course => (
+      <Course key={course.id} course={course} />
+    ))}
+    </div>
+    )
 
   
 }
@@ -51,10 +75,10 @@ const Course = ({course}) => {
 }
 
 //Used to display the mapped array of the course parts
-const Content = (props) => {
+const Content = ({part, exercises}) => {
   return(
   <div>
-      <p>{props.part} {props.exercises}</p>
+      <p>{part} {exercises}</p>
   </div>
   )
 

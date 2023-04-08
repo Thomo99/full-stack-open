7ca6faf83bb37bf5b8e8221c4c8427aed104ec9b
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-08 10:10:02 +1000

    List adds correctly.

--- part2/theory1/src/App.js
+++ part2/theory1/src/App.js
@@ -1,16 +1,41 @@
 import Note from './components/Note'
-
-const App = ({ notes }) => {
+import {useState, useTransition} from 'react'
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  //add notes
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
   return (
     <div>
       <h1>Notes</h1>
-      <ul>
         <ul>
           {notes.map(note => 
             <Note key={note.id} note={note} />
           )}
         </ul>
-      </ul>
+        <form onSubmit={addNote}>
+          <input 
+          value={newNote}
+          onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form>
     </div>
   )
 }

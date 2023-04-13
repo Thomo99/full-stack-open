0094ad41ed23bb5b33e501514d6a051b20213d8a
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-14 05:03:04 +1000

    notes.js implemented

--- part2/theory1/src/App.js
+++ part2/theory1/src/App.js
@@ -9,13 +9,11 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect (() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
+    })
   }, [])
   console.log('render', notes.length, 'notes')
 
@@ -26,22 +24,21 @@ const App = () => {
       content: newNote,
       important: Math.random() < 0.5,
     }
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then((response) => {
-        console.log(response)
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
       })
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
   }
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-
-    axios.put(url, changedNote).then(response => {
-    setNotes(notes.map(n => n.id !== id ? n : response.data))
-  })
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
   }
 
   const handleNoteChange = (event) => {

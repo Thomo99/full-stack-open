9fc2d5a331e0a40d6e8e77e5076db8d3f85af0e0
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-26 05:00:34 +1000

    Up to sending error 404. Not working yet though

--- part3/theory1/index.js
+++ part3/theory1/index.js
@@ -19,18 +19,29 @@ let notes = [
       important: true
     }
   ]
-  // const app = http.createServer((request, response) => {
-  //   response.writeHead(200, { 'Content-Type': 'application/json' })
-  //   response.end(JSON.stringify(notes))
-  // })
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
+  // app.get('/api/notes', (request, response) => {
+  //   response.json(notes)
+  // })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => {
+      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+      return note.id === id
+    })
+    console.log(note)
+    if(note){
+      response.json(note)
+    } else {
+      resposne.status(404).end()
+    }
+  }) 
 
 const PORT = 3001
 app.listen(PORT)

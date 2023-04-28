0b31cc2e108688f060093418a70cc21a228e09b2
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-29 05:17:06 +1000

    Theory part 1 complete

--- part3/theory1/index.js
+++ part3/theory1/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 // const http = require('http')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -19,30 +21,62 @@ let notes = [
       important: true
     }
   ]
-
+//gets
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-  // app.get('/api/notes', (request, response) => {
-  //   response.json(notes)
-  // })
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
 
   app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => {
-      console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-      return note.id === id
-    })
-    console.log(note)
-    if(note){
+    const note = notes.find(note => note.id === id)
+  
+    if (note) {
       response.json(note)
     } else {
-      resposne.status(404).end()
+      response.status(404).end()
     }
-  }) 
+  
+    response.json(note)
+  })
 
+  //post
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+  //delete
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)

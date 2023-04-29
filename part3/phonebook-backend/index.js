9e3e73fca641d35e97ca05918bff80c0fa0ce4b1
Thomo99 <114318806+Thomo99@users.noreply.github.com>
2023-04-29 20:49:08 +1000

    Search by id complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -49,6 +49,17 @@ app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people \n ${formattedDate}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id ===id)
+
+  if (person){
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 
 const PORT = 3001
 app.listen(PORT)
